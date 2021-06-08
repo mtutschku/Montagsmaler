4d53732c1a25f388c295e01d051162a9ec7a561c
Pascal Uhlendorff <pascaluhlendorff@gmx.de>
2021-06-08 17:42:05 +0200

    nicht wirklich funktional

--- components/gui/GUI.java
+++ components/gui/GUI.java
@@ -9,7 +9,8 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /** Grafisches Benutzerinterface für die Zeichnung, welche vom neuronalen Netzwerk erkannt werden soll.
@@ -23,67 +24,139 @@ import javafx.stage.Stage;
  */
 public class GUI extends Application {
 
+    String mode = "Paint";
     Button button;
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Paint It");
-        stage.setResizable(false);
-
-        //Buttons are not used yet
-        /*
-        Button buttonUndo = new Button();
-        buttonUndo.setText("UNDO");
-
-        Button buttonNextWord = new Button("NEXT");
-
-        Button buttonEraser = new Button("ERASER");
-
-        HBox buttons = new HBox();
-        buttons.getChildren().addAll(buttonUndo, buttonEraser, buttonNextWord);
-        */
-
+        
         Canvas canvas = new Canvas(840, 840);
         final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
         //event handler for mouse input
-        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, 
-            new EventHandler<MouseEvent>() {
+        if(mode.equals("Paint")) {
+            canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, 
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {            
+                        graphicsContext.beginPath();
+                        graphicsContext.moveTo(event.getX(), event.getY());
+                        graphicsContext.stroke();
+                    }
+            });
+
+            canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, 
+                new EventHandler<MouseEvent>(){
+                    @Override
+                    public void handle(MouseEvent event) {
+                        graphicsContext.lineTo(event.getX(), event.getY());
+                        graphicsContext.stroke();
+                        graphicsContext.closePath();
+                        graphicsContext.beginPath();
+                        graphicsContext.moveTo(event.getX(), event.getY());
+                    }
+            });
+
+            canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, 
+                new EventHandler<MouseEvent>(){
+                    @Override
+                    public void handle(MouseEvent event) {
+                        graphicsContext.lineTo(event.getX(), event.getY());
+                        graphicsContext.stroke();
+                        graphicsContext.closePath();
+                }
+            });  
+        } else if(mode.equals("Eraser")) {
+            graphicsContext.setFill(Color.WHITE);
+            canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, 
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {            
+                        graphicsContext.beginPath();
+                        graphicsContext.moveTo(event.getX(), event.getY());
+                        graphicsContext.stroke();
+                    }
+            });
+
+            canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, 
+                new EventHandler<MouseEvent>(){
+                    @Override
+                    public void handle(MouseEvent event) {
+                        graphicsContext.lineTo(event.getX(), event.getY());
+                        graphicsContext.stroke();
+                        graphicsContext.closePath();
+                        graphicsContext.beginPath();
+                        graphicsContext.moveTo(event.getX(), event.getY());
+                    }
+            });
+
+            canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, 
+                new EventHandler<MouseEvent>(){
+                    @Override
+                    public void handle(MouseEvent event) {
+                        graphicsContext.lineTo(event.getX(), event.getY());
+                        graphicsContext.stroke();
+                        graphicsContext.closePath();
+                }
+            });  
+        }
+
+        //initialize buttons
+        Button buttonNew = new Button("New");
+        Button buttonPaint = new Button("Paint");
+        Button buttonEraser = new Button("Eraser");
+        Button buttonUndo = new Button("Undo");
+        Button buttonNextWord = new Button("Next");
+    
+        //horizontal allignment of all buttons
+        HBox buttons = new HBox();
+        buttons.getChildren().addAll(buttonNew, buttonPaint, buttonEraser, buttonUndo, buttonNextWord);
+        
+        //event handler for buttons
+        buttonNew.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                graphicsContext.clearRect(0, 0, 840, 840);
+            }
+        });
+
+        buttonPaint.setOnAction(new EventHandler<ActionEvent>() {
+            @Override 
+            public void handle(ActionEvent event) {
+                mode = "Paint";
+            }
+        });
+
+        buttonEraser.setOnAction(new EventHandler<ActionEvent>() {
+            @Override 
+            public void handle(ActionEvent event) {
+                mode = "Eraser";
+            }
+        });
+
+        buttonUndo.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
                 
-                @Override
-                public void handle(MouseEvent event) {            
-                    graphicsContext.beginPath();
-                    graphicsContext.moveTo(event.getX(), event.getY());
-                    graphicsContext.stroke();
-                }
+            }
+        });
+        
+        buttonNextWord.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                
+            }
         });
 
-        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, 
-            new EventHandler<MouseEvent>(){
+        //sets the scene/stage and shows it
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(buttons);
+        borderPane.setCenter(canvas);
 
-                @Override
-                public void handle(MouseEvent event) {
-                    graphicsContext.lineTo(event.getX(), event.getY());
-                    graphicsContext.stroke();
-                    graphicsContext.closePath();
-                    graphicsContext.beginPath();
-                    graphicsContext.moveTo(event.getX(), event.getY());
-                }
-        });
+        Scene scene = new Scene(borderPane);
 
-        canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, 
-            new EventHandler<MouseEvent>(){
-
-                @Override
-                public void handle(MouseEvent event) {
-                    graphicsContext.lineTo(event.getX(), event.getY());
-                    graphicsContext.stroke();
-                    graphicsContext.closePath();
-               }
-        });    
-
-        Pane pane = new Pane(canvas);
-        Scene scene = new Scene(pane);
+        stage.setTitle("Paint It");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
