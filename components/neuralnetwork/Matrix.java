43130dee888fbd5f7c58e139c5faa66a5996188b
Morris Tutschku <morris-taylor@gmx.net>
2021-06-08 12:18:27 +0200

    Klasse hinzugefügt

---
+++ components/neuralnetwork/Matrix.java
@@ -0,0 +1,16 @@
+package components.neuralnetwork;
+
+/** Matrix-Objekt für das neuronale Netzwerk.
+ * 
+ * Beinhaltet Funktionen zum Rechnen mit Matrizen, die für das Netzwerk benötigt werden.
+ * 
+ * @version 8. Juni 2021
+ * @author Morris Tutschku
+ */
+public class Matrix {
+    
+    public Matrix(){
+        
+    }
+
+}
