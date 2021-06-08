f5e24174ca9a879457fd9a81b7d9077f1d558efb
Morris Tutschku <morris-taylor@gmx.net>
2021-06-08 21:40:02 +0200

    Netzwerk Grundgerüst

--- components/neuralnetwork/Network.java
+++ components/neuralnetwork/Network.java
@@ -1,5 +1,95 @@
 package components.neuralnetwork;
 
+/** Neuronales Netzwerk.
+ * 
+ * Funktionen zum Bedienen, Trainieren und Modifizieren des Netzwerks.
+ * Das Netzwerk hat zunächst eine Input-Layer (= Grauwerte der Leinwandpixel), eine Hidden-Layer und
+ * Output-Layer (= Klassifikation).
+ * 
+ * @version 8. Juni 2021
+ * @author Morris Tutschku
+ */
 public class Network {
-    
+
+    /** Anzahl Inputs */
+    int INPUT_SIZE;
+
+    /** Anzahl Neuronen in der Hidden-Layer */
+    int HIDDEN_SIZE;
+
+    /** Anzahl Outputs (= Anzahl mögl. Klassifikationen) */
+    int OUTPUT_SIZE;
+
+    /** Matrix für Gewichte zwischen Input-Layer und Hidden-Layer */
+    Matrix weightsIH;
+
+    /** Matrix für Gewichte zwischen Hidden-Layer und Output-Layer */
+    Matrix weightsHO;
+
+    /** Matrix für bias der Hidden-Layer-Neuronen */
+    Matrix biasH;
+
+    /** Matrix für bias der Output-Layer-Neuronen */
+    Matrix biasO;
+
+    /** Konstruktor
+     * 
+     * Initialisiert die Architektur des Netzwerks. Erzeugt zufällige Gewichts- und bias-Matrizen.
+     * 
+     * @param in Anzahl Inputs
+     * @param hidden Anzahl Hidden-Layer-Neuronen
+     * @param out Anzahl Outputs
+     */
+    public Network(int in, int hidden, int out){
+        if(in <= 0 || hidden <= 0 || out <= 0){
+            System.err.println("Ungültiges Netzwerk-Layout");
+            return;
+        }
+        
+        INPUT_SIZE = in;
+        HIDDEN_SIZE = hidden;
+        OUTPUT_SIZE = out;
+        weightsIH = new Matrix(HIDDEN_SIZE, INPUT_SIZE);
+        weightsHO = new Matrix(OUTPUT_SIZE, HIDDEN_SIZE);
+        biasH = new Matrix(HIDDEN_SIZE, 1);
+        biasO = new Matrix(OUTPUT_SIZE, 1);
+
+        // Gewichte zufällig initialisieren
+        weightsIH.randomize();
+        weightsHO.randomize();
+        biasH.randomize();
+        biasO.randomize();
+    }
+
+    /** Gibt alle Informationen zum Netzwerk auf der Konsole aus. */
+    public void print(){
+        System.out.println("\nweightsIH:");
+        weightsIH.print();
+        System.out.println("\nweightsHO:");
+        weightsHO.print();
+    }
+
+    /** Aktivierungsfunktion für das Netzwerk.
+     * 
+     * Hier wird die Sigmoidfunktion verwendet.
+     * sigmoid(x) := 1/(1+e^-x)
+     *  
+     * @param x Input
+     * @return Output (sigmoid(x))
+     */
+    public static double sigmoid(double x){
+        return 1.0/(1.0 + Math.exp(-x));
+    }
+
+    /** Funktion zum Verarbeiten einer Inputmatrix (= Pixel der Leinwand). */
+    public void feedForward(Matrix input){
+        if(input.getRows() != INPUT_SIZE || input.getCols() != 1){
+            System.err.println("Ungültiger Input. (Muss" + INPUT_SIZE + "x1 Matrix sein)");
+            return;
+        }
+        
+        // TODO
+
+    }
+
 }
