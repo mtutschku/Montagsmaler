f5f3f0934596d07f966d54c14f0b7efc61b020a3
jakobhima <75522692+jakobhima@users.noreply.github.com>
2021-07-01 18:01:17 +0200

    GUI Setup beendet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,6 @@ import javafx.application.Application;
 public class Main {
     
     public static void main(String[] args) throws IOException {
-        
-        
-        
         ////////////////////////////
         Network n = new Network(2,4,1);
         
@@ -45,55 +42,20 @@ public class Main {
         System.out.println("\n" + NetworkStats.accuracy); // nur Genauigkeit als Wert bekommen.
                                                           // Dafür muss aber zuerst getTrainingStats() ausgeführt werden,
                                                           // damit accuracy upgedated wird.
-        //Application.launch(NetworkPlot.class, args);
-
-
 
         ////////////////////////////
         // @author Jakob Hiestermann
         
-        
-        /** holds constant value m that defines the mxm matrix size used in this project */
+        /** holds constant value m that defines the mxm matrix size used in this project. */
         final int M = 28;
 
         /** GUI setup and launch */
         Handler handler = new Handler(M, M);
-        Network network = new Network(M*M, 4, 7); // for now an arbitrary number of hidden layers (and outputs) chosen, to be changed adequately 
+        Network network = new Network(M*M, 2, 7); // for now an arbitrary number of hidden layers (and outputs) chosen, to be changed adequately 
                                                 //TODO: change values 
         GUI.setHandler(handler);
         GUI.setNetwork(network);
         Application.launch(GUI.class, args);
-        
-        
-        // Data translatedInput;
-        // Matrix networkGuessM;
-        // String networkGuess;
-        
-        
-        // BufferedImage image = null;
-        // File f = null;
-        
-        // try {
-        //     f = new File(D_LOCATION);
-        //     image = new BufferedImage(IM_DIMENSION, IM_DIMENSION, BufferedImage.TYPE_INT_ARGB);
-        //     image = ImageIO.read(f);
-        // } catch(IOException e) {
-        //     System.out.println("Error: " + e);
-        // }
-        
-        // translatedInput = handler.translateImage(image);
-        // translatedInput.getInputs().print();
-
-        // // uncomment this, as soon as network has been trained and can be fed actual drawings
-        //     // networkGuessM = network.feedForward(translatedInput.getInputs());
-        //     // networkGuess = Meta.getCertainMETA(networkGuessM.getHighestValueRow());
-        //     // GUI.setGuessLabelText(networkGuess);
-
-        // // uncomment to check Network Guess Matrix
-        // // networkGuessM.print();
-        
-        ///////////////////////////////////
-
     }
 
 }
